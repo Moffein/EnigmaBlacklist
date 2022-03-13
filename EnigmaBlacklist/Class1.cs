b3824482cfaa3b46e46027b5943eaa784bab0200
Moffein <22742925+Moffein@users.noreply.github.com>
2022-03-13 11:03:26 -0700

    Updated Whitelist so that it adds all whitelisted equipment to enigma.

--- EnigmaBlacklist/Class1.cs
+++ EnigmaBlacklist/Class1.cs
@@ -8,7 +8,7 @@ using UnityEngine;
 
 namespace EnigmaBlacklist
 {
-    [BepInPlugin("com.Moffein.EnigmaBlacklist", "Enigma Blacklist", "1.0.1")]
+    [BepInPlugin("com.Moffein.EnigmaBlacklist", "Enigma Blacklist", "1.0.2")]
     public class EnigmaBlacklist : BaseUnityPlugin
     {
         public static bool blacklistLunars = true;
@@ -22,7 +22,7 @@ namespace EnigmaBlacklist
         private void ReadConfig()
         {
             blacklistLunars = Config.Bind("Settings", "Blacklist Lunars", true, "Prevent Lunars from being rolled.").Value;
-            blacklistString = Config.Bind("Settings", "Blacklist String", "Recycle, Tonic, BossHunter, BossHunterConsumed", "Equipments to blacklist. Separated by comma, case-sensitive. Full list can be found at https://github.com/risk-of-thunder/R2Wiki/wiki/Item-&-Equipment-IDs-and-Names").Value;
+            blacklistString = Config.Bind("Settings", "Blacklist String", "Recycle, Tonic, BossHunter, BossHunterConsumed, GoldGat", "Equipments to blacklist. Separated by comma, case-sensitive. Full list can be found at https://github.com/risk-of-thunder/R2Wiki/wiki/Item-&-Equipment-IDs-and-Names").Value;
             useWhitelist = Config.Bind("Settings", "Use Whitelist", false, "Use a whitelist instead of a blacklist. Disables Blacklist String setting.").Value;
             whitelistString = Config.Bind("Settings", "Whitelist String", "", "Equipments to whitelist. Separated by comma, case-sensitive. Full list can be found at https://github.com/risk-of-thunder/R2Wiki/wiki/Item-&-Equipment-IDs-and-Names").Value;
         }
@@ -81,6 +81,18 @@ namespace EnigmaBlacklist
                 foreach (EquipmentIndex ei in toRemove)
                 {
                     EnigmaArtifactManager.validEquipment.Remove(ei);
+                }
+
+                //Add whitelisted equipments to engima
+                if (useWhitelist)
+                {
+                    foreach (EquipmentIndex ei in whitelist)
+                    {
+                        if (!EnigmaArtifactManager.validEquipment.Contains(ei))
+                        {
+                            EnigmaArtifactManager.validEquipment.Add(ei);
+                        }
+                    }
                 }
             };
         }
